66c4134a90c96167a750f76b7e522e691bea7b37
Luis Vargas <lvarg046@knights.ucf.edu>
2022-01-17 00:58:14 -0500

    Contact.php works!

--- forms/contact.php
+++ forms/contact.php
@@ -1,36 +1,36 @@
 <?php
 
 
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\Exception;
-//require 'forms/vendor/autoload.php';
-//$mail = new PHPMailer(true);
-//$mail->isHTML(true);
-//
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require_once "../vendor/autoload.php";
+$mail = new PHPMailer(true);
+$mail->isHTML(true);
+
 $fromName = $_POST['name'];
 $fromEmail = $_POST['email'];
 $subject = $_POST['subject'];
-$sendToEmail = 'sigorel286@dkb3.com';
-//$sendToName = 'Luis Vargas';
+$sendToEmail = 'dakefiy638@dkb3.com';
+$sendToName = 'Luis Vargas';
 $message = $_POST['message'];
 
-$headers = array(
-    'From' => $fromName,
-    'Reply-To'=> $fromEmail,
-    'X-Mailer'=>'PHP/'.phpversion()
-);
-mail($sendToEmail, $subject, $message, $headers);
+try {
+    $mail->setFrom($fromEmail,$fromName);
+    $mail->addAddress($sendToEmail, $sendToName);
+    $mail->addReplyTo($fromEmail, $fromName);
+    $mail->Subject = $subject;
+    $mail->msgHTML($message);
+    $mail->send();
+    echo "Your message has been sent. Thank you!";
+} catch (Exception $e) {
+    echo "Error sending message. Please try again";
+}
+
+//$headers = array(
+//    'From' => $fromName,
+//    'Reply-To'=> $fromEmail,
+//    'X-Mailer'=>'PHP/'.phpversion()
+//);
+//mail($sendToEmail, $subject, $message, $headers);
 //
-//try {
-//    $mail->setFrom($fromEmail,$fromName);
-//    $mail->addAddress($sendToEmail, $sendToName);
-//    $mail->addReplyTo($fromEmail, $fromName);
-//    $mail->Subject = $subject;
-//    $mail->msgHTML($message);
-//    echo $mail->send();
-//} catch (Exception $e) {
-//    echo "ERROR Please try again";
-//}
-
-
 ?>
