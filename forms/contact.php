8967778e204af175412218cf11ff20879f6971bc
Luis Vargas <32406288+lvarg046@users.noreply.github.com>
2022-01-16 19:32:13 -0500

    Working through trying to get  php to send email

--- forms/contact.php
+++ forms/contact.php
@@ -1,1 +1,36 @@
 <?php
+
+
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\Exception;
+//require 'forms/vendor/autoload.php';
+//$mail = new PHPMailer(true);
+//$mail->isHTML(true);
+//
+$fromName = $_POST['name'];
+$fromEmail = $_POST['email'];
+$subject = $_POST['subject'];
+$sendToEmail = 'sigorel286@dkb3.com';
+//$sendToName = 'Luis Vargas';
+$message = $_POST['message'];
+
+$headers = array(
+    'From' => $fromName,
+    'Reply-To'=> $fromEmail,
+    'X-Mailer'=>'PHP/'.phpversion()
+);
+mail($sendToEmail, $subject, $message, $headers);
+//
+//try {
+//    $mail->setFrom($fromEmail,$fromName);
+//    $mail->addAddress($sendToEmail, $sendToName);
+//    $mail->addReplyTo($fromEmail, $fromName);
+//    $mail->Subject = $subject;
+//    $mail->msgHTML($message);
+//    echo $mail->send();
+//} catch (Exception $e) {
+//    echo "ERROR Please try again";
+//}
+
+
+?>
